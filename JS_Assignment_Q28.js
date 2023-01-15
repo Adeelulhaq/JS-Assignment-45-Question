60e590bc3b775485f06683ada7860eb63e4336d5
Adeelulhaq <adeeulhaq@gmail.com>
2023-01-15 13:52:39 +0500

    Update JS_Assignment_Q28.js

---
+++ JS_Assignment_Q28.js
@@ -0,0 +1,16 @@
+
+let l_age = 2;
+
+if (l_age < 2) {
+    console.log("The person is a baby.");
+} else if (l_age >= 2 && l_age < 4) {
+    console.log("The person is a toddler.");
+} else if (l_age >= 4 && l_age < 13) {
+    console.log("The person is a kid.");
+} else if (l_age >= 13 && l_age < 20) {
+    console.log("The person is a teenager.");
+} else if (l_age >= 20 && l_age < 65) {
+    console.log("The person is an adult.");
+} else {
+    console.log("The person is an elder.");
+}
